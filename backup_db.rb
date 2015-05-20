1bf7c889e91638cf87162dc38a25150b7bee431e
Jordaniel Wolk <jd@gophilosophie.com>
2015-05-20 00:00:47 -0700

    First (last?) commit

---
+++ backup_db.rb
@@ -0,0 +1,31 @@
+require 'fileutils'
+
+class Archiver < Struct.new(:dest_dir, :src_file)
+  def archive!
+    puts "Creating DB backup in #{dest_file}"
+    FileUtils.cp src_file, dest_file
+  end
+
+  private
+
+  def dest_file
+    src_filename = make_filename(File.basename(src_file))
+    File.join(dest_dir, src_filename)
+  end
+
+  def make_filename(orig_file)
+    ext = File.extname(orig_file)
+    filename = File.basename(orig_file, ext)
+    "#{filename}__#{Time.now.strftime('%Y_%m_%d-%H_%M_%S')}#{ext}"
+  end
+end
+
+# TODO: Dummy hardcoded paths - change in real usage
+dest_dir = '/path/to/DB/Archive'
+backend_file = '/path/to/DB_be.accdb'
+frontend_file = '/path/to/DB_fe.accdb'
+
+be_archiver = Archiver.new(dest_dir, backend_file)
+fe_archiver = Archiver.new(dest_dir, frontend_file)
+be_archiver.archive!
+fe_archiver.archive!
